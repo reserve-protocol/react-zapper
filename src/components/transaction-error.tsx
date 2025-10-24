ea40cf7b841a91250441f61062b18c78a617833c
Juan Pablo Rombola <juampi.rombola@gmail.com>
2025-10-24 15:56:52 -0300

    fix: error styles

--- src/components/transaction-error.tsx
+++ src/components/transaction-error.tsx
@@ -6,32 +6,24 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
   withName?: boolean
 }
 
-const TransactionError = ({ error, withName = true, className, ...props }: Props) => {
+const TransactionError = ({
+  error,
+  withName = true,
+  className,
+  ...props
+}: Props) => {
   if (!error) {
     return null
   }
-  let parsed = false
-
   const messageSplit = error.message.split('\n')
-  let message =
+  const message =
     messageSplit.length > 1
       ? messageSplit[0] + ' ' + messageSplit[1]
-      : (messageSplit[0] ?? '')
-
-  if (message.includes('0x168cdd18')) {
-    parsed = true
-    message =
-      'Proposal cannot be executed while another auction is currently running'
-  }
+      : messageSplit[0] ?? ''
 
   return (
     <div className={cn(className)} {...props}>
-      <p
-        className={cn(
-          "text-red-500 text-sm whitespace-pre-wrap",
-          parsed ? "break-words" : "break-all"
-        )}
-      >
+      <p className={cn('text-red-500 text-sm whitespace-pre-wrap break-all')}>
         {withName && `${error.name}:`}
         {withName && <br />}
         {message}
